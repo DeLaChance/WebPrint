d65b27921e0eb2a17b52cbbe03f7c4f4187c18f8
Lucien Valstar <ldjvalstar@gmail.com>
2017-11-20 15:23:30 +0100

    Added routing to webapp
    - Webapp has been divided into several sub-webapps.
    - Added an overview app.

--- webprint-frontend/src/app/app.module.ts
+++ webprint-frontend/src/app/app.module.ts
@@ -2,25 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { JoblistComponent } from './joblist/joblist.component';
 import { JobactivityComponent } from './jobactivity/jobactivity.component';
 import { DraganddropComponent } from './draganddrop/draganddrop.component';
+import { OverviewComponent } from './overview/overview.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { RestService } from './rest.service';
+
+const appRoutes: Routes = [
+  { path: 'new', component: DraganddropComponent },
+  { path: 'active', component: JobactivityComponent },
+  { path: 'list', component: JoblistComponent },
+  { path: '', component: OverviewComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     JoblistComponent,
     JobactivityComponent,
-    DraganddropComponent
+    DraganddropComponent,
+    PageNotFoundComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
     DataTablesModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [ RestService ],
   bootstrap: [AppComponent]
